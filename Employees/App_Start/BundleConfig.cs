9d24ca03cbf105c280d6c77c610897d9f5232c43
Тимур Салимзянов <tsalimzyanov@artsofte.ru>
2015-10-25 13:42:56 +0500

    Добавил бандлы для скриптов

---
+++ Employees/App_Start/BundleConfig.cs
@@ -0,0 +1,18 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace Employees
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/js/jquery").Include(
+                "~/Scripts/jquery-{version}.js",
+                "~/Scripts/jquery-ui-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/js/Employees/autocomplite").Include(
+                "~/Scripts/Employees/Autocomplite.js"));
+        }
+    }
+}
--- Employees/Global.asax.cs
+++ Employees/Global.asax.cs
@@ -4,6 +4,7 @@ using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 using Employees.Models;
 
@@ -17,6 +18,7 @@ namespace Employees
 
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
